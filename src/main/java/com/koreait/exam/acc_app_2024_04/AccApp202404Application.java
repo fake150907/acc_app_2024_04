73d534b630e619d2343839baf91c66559871571c
fake150907 <yungayeon223@gmail.com>
2024-04-26 16:22:41 +0900

    스프링 시큐리티, 로그인, 로그아웃, 회원가입 2

--- src/main/java/com/koreait/exam/acc_app_2024_04/AccApp202404Application.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/AccApp202404Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,4 +19,5 @@ public class AccApp202404Application {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
